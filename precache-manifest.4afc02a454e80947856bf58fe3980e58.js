d07b9b3346328d37f51a6513263775aa139acd25
Bryan Meneses <bryan@Bryans-MacBook-Pro.local>
2019-05-10 20:58:29 -0700

    Updates

---
+++ precache-manifest.4afc02a454e80947856bf58fe3980e58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f796d8e9fcdf01c78ef4874d23c6a55",
+    "url": "/kittyFriends/index.html"
+  },
+  {
+    "revision": "c8f481393ca8b7f60576",
+    "url": "/kittyFriends/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "4deeba31fcceaf96f87f",
+    "url": "/kittyFriends/static/css/main.4126a6eb.chunk.css"
+  },
+  {
+    "revision": "c8f481393ca8b7f60576",
+    "url": "/kittyFriends/static/js/2.53e8458a.chunk.js"
+  },
+  {
+    "revision": "4deeba31fcceaf96f87f",
+    "url": "/kittyFriends/static/js/main.4b826d06.chunk.js"
+  },
+  {
+    "revision": "c5cf26c3432d04fb345f",
+    "url": "/kittyFriends/static/js/runtime~main.595fb9f0.js"
+  }
+]);
